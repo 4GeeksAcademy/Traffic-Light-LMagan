bb5461c948e21d039830046b2f4f2abf856a3dc5
L.Magán <lucasfliper@hotmail.com>
2024-12-11 22:11:59 +0000

    first-commit

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useState } from "react";
+
+
+const TrafficLight = () => {
+  const [ color, setColor] = useState("red");
+
+    return  (<div className="semaforo">
+        <div className=" text-center" style={{width: "80px", height: "220px", background: "black"}}>
+        
+        <button onClick={()=>setColor("red")} className={`red` + (color === "red" ? " iluminado" : "" )}></button>
+        <button onClick={()=>setColor("yellow")} className={`yellow` + (color === "yellow" ? " iluminado" : "" )}></button>
+        <button onClick={()=>setColor("green")} className={`green` + (color === "green" ? " iluminado" : "" )}></button>
+      </div>
+      </div>
+      
+  );
+  };
+//<button onClick={()=>}>Click me!</button>
+export default TrafficLight;
